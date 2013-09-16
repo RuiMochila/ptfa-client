5f893ec457daf6d9e7824134bf246cef7ef674af
RuiMochila <rui.mochila@gmail.com>
2013-09-16 11:04:27 +0100

    Tinha ido aqui uma coisa a mais

--- platforms/android/bin/PortugalFestivalAwards-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/ruimochila/Cordova/ptfa-client/platforms/android/bin/PortugalFestivalAwards-debug-unaligned.apk : \
-/Users/ruimochila/Cordova/ptfa-client/platforms/android/bin/PortugalFestivalAwards.ap_ \
-/Users/ruimochila/Cordova/ptfa-client/platforms/android/bin/classes.dex \
-/Users/ruimochila/Cordova/ptfa-client/platforms/android/libs/cordova-3.0.0.jar \
